6c7b50e66f781ea4a3699aa157c6a53219a7727e
David <nodearth@gmail.com>
2016-08-24 09:14:53 +0100

    Removed comment

--- example1.js
+++ example1.js
@@ -1,6 +1,6 @@
 const got = require('got')
 const parse = require('jsonstream2').parse
-// TODO David: Callback vs stream
+
 got.stream('http://registry.npmjs.org/-/all')
    .pipe(parse('*.name', (name) => name + '\n'))
    .pipe(process.stdout)
